947323acc238fc746bf32386016642125adb2511
Esk1z0 <j.esteban.rincon.m@gmail.com>
2025-12-07 15:49:16 +0100

    funcion rastringin implementada

--- pso_proyect/src/objective_functions.py
+++ pso_proyect/src/objective_functions.py
@@ -1,3 +1,9 @@
-def rastrigin(x)
-def sphere(x)  # Para otros experimentos
-def rosenbrock(x)  # Para otros experimentos
+import numpy as np
+
+def rastrigin (x) :
+    assert all(abs(xi)<=5.12 for xi in x)
+    A = 10
+    return A * len(x) + sum([ (xi**2 - A * np.cos(2*np.pi*xi)) for xi in x ])
+
+
+    
